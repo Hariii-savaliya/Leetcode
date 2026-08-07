c4584fc04c1c335943233ca463ae90e1f5550ff5
Harvish Savaliya <harvishsavaliya1234@gmail.com>
2026-08-07 07:33:38 +0530

    Time: 0 ms (100%), Space: 12.5 MB (0.39%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        a = 0
+        l = 0
+        for ch in s:
+            if ch == "A":
+                a += 1
+                if a >= 2:
+                    return False
+            if ch == "L":
+                l += 1
+                if l >= 3:
+                    return False
+            else: l = 0
+        return True
